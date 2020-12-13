674717756707de6218d228d3ea4a38c5bfbfca31
jakosv <jakosvadim@gmail.com>
2020-12-14 00:58:07 +0300

    Code cleaning

--- phonebook.c
+++ phonebook.c
@@ -3,58 +3,55 @@
 #include <stdio.h>
 #include <string.h>
 
-void addContact(const char* fileName, Contact* contact, SortParam param) {
+void addContact(const char* fileName, const Contact* contact, const SortParam param) {
     FILE* file = fopen(fileName, "ab");
     // Write line in file. Format: "namenumber\n"
     fwrite(contact, sizeof(Contact), 1, file);
     fputc('\n', file);
+    fclose(file);
+
     if (param == WithSorting) {
         sortContacts(fileName);
     }
-    fclose(file);
 }
 
 // Removes contact from file by phone number
 void removeContact(const char* fileName, const char* phoneNumber) {
-    FILE* file = fopen(fileName, "rb");
     // If file does not exist, then terminate program
-    checkFileExists(file, fileName);
+    checkFileExists(fileName);
     Contact* contacts = getContacts(fileName);
-    fclose(file);
-
     int rows = countContacts(fileName);
+
     clearPhonebook(fileName);
     for (int i = 0; i < rows; ++i) {
         if (strcmp(contacts[i].number, phoneNumber) != 0) {
             addContact(fileName, contacts + i, WithoutSorting);
         }
     }
+
+    free(contacts);
 }
 
 // Edits contact in file by old phone number
 void editContact(const char* fileName,
                  const char* phoneNumber,
-                 Contact* newContact)
+                 const Contact* newContact)
 {
-    FILE* file = fopen(fileName, "rb");
-    checkFileExists(file, fileName);
-    fclose(file);
+    checkFileExists(fileName);
     removeContact(fileName, phoneNumber);
     addContact(fileName, newContact, WithSorting);
 }
 
 // Prints sorted array of contacts
 void printContacts(const char* fileName) {
-    FILE* file = fopen(fileName, "rb");
-    checkFileExists(file, fileName);
-    sortContacts(fileName);
+    checkFileExists(fileName);
     int rows = countContacts(fileName);
+    sortContacts(fileName);
     Contact* contacts = getContacts(fileName);
+
     for (int i = 0; i < rows; ++i) {
         printf("%s %s\n", contacts[i].name, contacts[i].number);
     }
-    
-    fclose(file);
 }
 
 // Clears all file 
@@ -66,10 +63,11 @@ void clearPhonebook(const char* fileName) {
 
 // Return count of rows in file
 int countContacts(const char* fileName) {
+    checkFileExists(fileName); // if file doesn't exist then exit(1);
     FILE* file = fopen(fileName, "rb");
-    checkFileExists(file, fileName); // if file doesn't exist then exit(1);
     int count = 0;
     Contact temp;
+    
     fread(&temp, sizeof(Contact), 1, file);
     fgetc(file); // reads '\n'
     // reads lines while does not reach end of file
@@ -79,32 +77,39 @@ int countContacts(const char* fileName) {
         fgetc(file); // reads '\n'
         ++count;
     }
+    
     fclose(file);
+
     return count;
 }
 
 // Returns contacts array from file
 Contact* getContacts(const char* fileName) {
+    checkFileExists(fileName); // if file doesn't exist then exit(1);
     int rows = countContacts(fileName);
     FILE* file = fopen(fileName, "rb");
-    checkFileExists(file, fileName); // if file doesn't exist then exit(1);
     Contact* contactsArray = (Contact*) malloc(sizeof(Contact) * rows);
+
     for (int i = 0; i < rows; ++i) {
         // Reads line. Format: "namenumber\n"
         fread(contactsArray + i, sizeof(Contact), 1, file);
         fgetc(file); // reads '\n'
     }
+
     fclose(file);
+
     return contactsArray;
 }
 
+// Sorts lines in file
 void sortContacts(const char* fileName) {
     Contact* contacts = getContacts(fileName);
     int rows = countContacts(fileName);
     clearPhonebook(fileName);
     Contact temp;
-    for (int j = rows - 1; j > 0; j--) {
-        for (int i = 0; i < j; i++) {
+
+    for (int j = rows - 1; j > 0; --j) {
+        for (int i = 0; i < j; ++i) {
             if (strcmp(contacts[i].name, contacts[i + 1].name) > 0) {
                 temp = contacts[i];
                 contacts[i] = contacts[i + 1];
@@ -116,14 +121,18 @@ void sortContacts(const char* fileName) {
     for (int i = 0; i < rows; ++i) {
         addContact(fileName, contacts + i, WithoutSorting);
     }
+
+    free(contacts);
 }
 
 // Checks file. If file does not exists, then terminate program
-void checkFileExists(const FILE* file, const char* fileName) {
+void checkFileExists(const char* fileName) {
+    FILE* file = fopen(fileName, "rb");
     if (!file) {
         printf("%s does not exist!\n", fileName);
         exit(1);
     }
+    fclose(file);
 }
 
 // Delets '\n' character in string
--- phonebook.h
+++ phonebook.h
@@ -16,17 +16,17 @@ typedef enum {
     WithSorting,
 } SortParam; 
 
-void addContact(const char* fileName, Contact* contact, SortParam param);
+void addContact(const char* fileName, const Contact* contact, const SortParam param);
 void removeContact(const char* fileName, const char* phoneNumber);
 void editContact(const char* fileName,
                  const char* phoneNumber,
-                 Contact* newContact);
+                 const Contact* newContact);
 void printContacts(const char* fileName);
 void clearPhonebook(const char* fileName);
 int countContacts(const char* fileName);
 Contact* getContacts(const char* fileName);
 void sortContacts(const char* fileName);
-void checkFileExists(const FILE* file, const char* fileName);
+void checkFileExists(const char* fileName);
 void trimString(const size_t size, char* str);
 
 #endif
